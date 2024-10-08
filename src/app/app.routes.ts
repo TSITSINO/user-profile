ddf815bab2730ae47a9a54f8b3e8f8281e6a383d
tsitsino <tsitsilashvilitsitsino@gmail.com>
2024-10-08 20:50:25 +0400

    Addrouting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: UserProfileComponent,
+  },
+  {
+    path: 'edit-profile',
+    loadComponent: () =>
+      import('./create-user-profile/edit-user-profile.component').then(
+        (m) => m.EditUserProfileComponent
+      ),
+  },
+];
